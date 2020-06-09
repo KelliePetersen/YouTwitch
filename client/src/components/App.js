491a039127c15f0bebe105d7b75787200502918d
KelliePetersen <kelliepetersen@outlook.com>
2020-06-09 10:21:53 +1000

    create app skeleton pages and links

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const pageOne = () => {
-  return (
-    <div>page one
-      <Link to="/pagetwo">page two</Link>
-    </div>
-  )
-}
-
-const pageTwo = () => {
-  return (
-    <div>page two
-      <Link to="/">page one</Link>
-    </div>
-  )
-}
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={pageOne}/>
-          <Route path="/pagetwo" component={pageTwo}/>
+          <Route path="/" exact component={StreamList}/>
+          <Route path="/streams/new" component={StreamCreate}/>
+          <Route path="/streams/edit" component={StreamEdit}/>
+          <Route path="/streams/delete" component={StreamDelete}/>
+          <Route path="/streams/show" component={StreamShow}/>
         </div>
       </BrowserRouter>
     </div>
