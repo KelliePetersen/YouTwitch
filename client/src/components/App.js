dc4b13d0b145b87be64efb9fb98500603e748c50
KelliePetersen <kelliepetersen@outlook.com>
2020-06-09 10:10:14 +1000

    add basic router logic

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const pageOne = () => {
+  return (
+    <div>page one
+      <Link to="/pagetwo">page two</Link>
+    </div>
+  )
+}
+
+const pageTwo = () => {
+  return (
+    <div>page two
+      <Link to="/">page one</Link>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={pageOne}/>
+          <Route path="/pagetwo" component={pageTwo}/>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
