b141e4334d93bfe84c03377cc8985adbe29d419c
KelliePetersen <kelliepetersen@outlook.com>
2020-06-11 18:38:51 +1000

    create Modal component and add to StraemDelete

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,34 +1,12 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { deleteStream } from '../../actions';
+import React from 'react';
+import Modal from '../Modal';
 
-export class StreamDelete extends Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
-  }
-
-  onSubmit = (formValues) => {
-    this.props.deleteStream(this.props.match.params.id, formValues)
-  }
-  render() {
-    if (!this.props.stream) {
-      return <div>Loading...</div>
-    }
-    return (
-      <div>
-        <h3>Delete Stream</h3>
-        <p>Are you sure you want to delete {this.props.stream.title}?</p>
-      </div>
-    )
-  }
+const StreamDelete = () => {
+  return (
+    <div>
+      <Modal />
+    </div>
+  )
 }
 
-const mapStateToProps = (state, ownProps) => {
-  return { stream: state.streams[ownProps.match.params.id]  };
-}
-
-const mapDispatchToProps = {
-  deleteStream
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(StreamDelete);
+export default StreamDelete;
