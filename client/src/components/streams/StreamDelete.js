23a9a03dac28b99493c4dafe3ddbe692ad694b93
KelliePetersen <kelliepetersen@outlook.com>
2020-06-11 17:48:25 +1000

    add code to fetch Stream in StreamDelete component

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { deleteStream } from '../../actions';
 
-const StreamDelete = () => {
-  return (
-    <div>
-      StreamDelete
-    </div>
-  )
+export class StreamDelete extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = (formValues) => {
+    this.props.deleteStream(this.props.match.params.id, formValues)
+  }
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>
+        <h3>Delete Stream</h3>
+        <p>Are you sure you want to delete {this.props.stream.title}?</p>
+      </div>
+    )
+  }
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id]  };
+}
+
+const mapDispatchToProps = {
+  deleteStream
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StreamDelete);
