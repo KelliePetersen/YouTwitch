9b8dfbce12e3d54037979cad69b9230a5504ce4b
KelliePetersen <kelliepetersen@outlook.com>
2020-06-11 18:55:03 +1000

    generalize modal and add customisation to modal in StreamDelete

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
+  const actions = (
+    <>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  )
+
   return (
     <div>
-      <Modal />
+      <Modal 
+        title="Delete Stream"
+        message="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push('/')}
+      />
     </div>
   )
 }
